2e808e3d4ceaba00a0e262ab5e71b91512a905ce
Rishu <115208147+XoRishu@users.noreply.github.com>
2023-04-10 18:57:34 +0530

    Update start.py

--- AnonX/utils/inline/start.py
+++ AnonX/utils/inline/start.py
@@ -19,7 +19,7 @@ def start_pannel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
                 callback_data="settings_back_helper",
             ),
             InlineKeyboardButton(
-                text="", callback_data="settings_helper"
+                text="Settings", callback_data="settings_helper"
             ),
         ],
      ]
@@ -30,26 +30,26 @@ def private_panel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
     buttons = [
         [
             InlineKeyboardButton(
-                text="ᴀᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ɢʀᴏᴜᴘ",
+                text="➕Add me to your group",
                 url=f"https://t.me/{BOT_USERNAME}?startgroup=true",
             )
         ],
         [
             InlineKeyboardButton(
-                text="ʜᴇʟᴩ", callback_data="settings_back_helper"
+                text="Help", callback_data="settings_back_helper"
             )
         ],
         [
             InlineKeyboardButton(
-                text="❣ sᴜᴩᴩᴏʀᴛ ❣", url=config.SUPPORT_GROUP
+                text="Support", url=config.SUPPORT_GROUP
             ),
             InlineKeyboardButton(
-                text="ᴍᴀɪɴᴛᴀɪɴᴇʀ", user_id=OWNER
+                text="~Bot Owner", user_id=OWNER
             )
         ],
         [
             InlineKeyboardButton(
-                text=" sᴏᴜʀᴄᴇ", url=config.UPSTREAM_REPO
+                text=" Source Code", url=config.UPSTREAM_REPO
             )
         ],
      ]
